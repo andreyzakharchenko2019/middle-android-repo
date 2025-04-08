f3b1310e58c7d79a20f1d70d8963d93bc64ed607
azakharcenko <azakharcenko@wooppay.com>
2025-04-08 23:56:57 +0500

    - add view on CustomContainer.kt

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
+import com.example.androidpracticumcustomview.R
 
 /*
 Задание:
@@ -20,22 +22,60 @@ class CustomContainer @JvmOverloads constructor(
 
     init {
         setWillNotDraw(false)
+        setBackgroundColor(ContextCompat.getColor(context, R.color.purple_500))
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+
+        var totalWidth = 0
+        var totalHeight = 0
+
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0)
+
+            // Суммируем размеры каждого дочернего элемента
+            totalWidth = maxOf(totalWidth, child.measuredWidth)
+            totalHeight += child.measuredHeight
+        }
+
+        // Применяем размеры
+        val width = resolveSize(totalWidth, widthMeasureSpec)
+        val height = resolveSize(totalHeight, heightMeasureSpec)
+
+        setMeasuredDimension(width, height)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+
+        if (childCount == 1) {
+
+            val child = getChildAt(0)
+
+            val childLeft = (right - left - child.measuredWidth) / 2
+            val childRight = childLeft + child.measuredWidth
+            val childBottom = top + child.measuredHeight
+
+            child.layout(childLeft, top, childRight, childBottom)
+        } else {
+
+            val child = getChildAt(1)
+
+            val childLeft = (right - left - child.measuredWidth) / 2
+            val childTop = bottom - child.measuredHeight
+            val childRight = childLeft + child.measuredWidth
+
+            child.layout(childLeft, childTop, childRight, bottom)
+        }
+
     }
 
     override fun addView(child: View) {
-       if ( this.childCount == 2) {
-           throw IllegalStateException("Элемента может быть только два")
-       }
+        if (this.childCount == 2) {
+            throw IllegalStateException("Элементов может быть только два")
+        }
+
+        super.addView(child)
     }
 }
